c589118e40dc05613aa26c14a0c4dfdd4ba7993a
lifeich1 <lifeich0@gmail.com>
2024-01-17 00:46:35 +0800

    :green_heart: fix armv7 build

--- hobob/src/main.rs
+++ hobob/src/main.rs
@@ -1,8 +1,8 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use hobob::{main_loop, prepare_log};
 
 async fn run() -> Result<()> {
-    prepare_log()?;
+    prepare_log().context("fail prepare log")?;
     main_loop().await?;
     Ok(())
 }
@@ -10,8 +10,8 @@ async fn run() -> Result<()> {
 #[tokio::main]
 async fn main() {
     if let Err(e) = run().await {
-        log::error!("FATAL: {e}");
-        panic!("Error at startup: {e}");
+        log::error!("FATAL: {e:#?}");
+        panic!("Error at startup: {e:?}");
     }
 
     log::info!("quit");
