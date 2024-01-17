3d285f813bc5a43d52ab8040cb3af0effd355dba
lifeich1 <lifeich0@gmail.com>
2024-01-18 00:30:44 +0800

    :construction: bench design

--- hobob/src/bench.rs
+++ hobob/src/bench.rs
@@ -8,21 +8,47 @@ pub enum DNode {
     Dir(HashMap<String, u64>),
     Index(OrdSet<(u64, u64)>),
 }
-#[derive(Default, Clone)]
+#[derive(Clone)]
 pub struct Bench {
     fs: HashMap<u64, DNode>,
 }
 
+impl Default for Bench {
+    fn default() -> Self {
+        let mut fs = HashMap::<u64, DNode>::default();
+        fs.insert(0, DNode::Dir(HashMap::default()));
+        Self { fs }
+    }
+}
+
 impl Bench {
+    #[must_use]
     pub fn get(&self, path: &Value) -> Option<&DNode> {
-        todo!()
+        Self::as_slice_path(path)
+            .ok()
+            .and_then(|p| self.slice_path_get(p))
     }
 
-    pub fn set(&self, path: &Value, content: Value) -> Result<()> {
-        todo!()
+    /// # Errors
+    /// TODO
+    pub fn set(&mut self, path: &Value, content: Value) -> Result<()> {
+        Self::as_slice_path(path).and_then(|p| self.slice_path_set(p, content))
     }
 
+    #[must_use]
     pub fn pull_log(&self, ack: i64) -> Vec<(u64, Value)> {
+        todo!()
+    }
+
+    fn slice_path_get(&self, path: &[Value]) -> Option<&DNode> {
+        todo!()
+    }
+
+    fn slice_path_set(&mut self, path: &[Value], content: Value) -> Result<()> {
+        todo!()
+    }
+
+    fn as_slice_path(path: &Value) -> Result<&[Value]> {
         todo!()
     }
 }
