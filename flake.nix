540d79c7e278b3cd0f0748558c141ceb50d97e43
lifeich1 <lifeich0@gmail.com>
2024-06-15 05:48:11 +0800

    :construction_worker: nix flake pack

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "An web app for hobo kicked from bibi&lili";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , utils
+    , ...
+    }:
+    utils.lib.eachDefaultSystem
+      (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          toolchain = pkgs.rustPlatform;
+          deps = with pkgs; [
+            openssl
+            sqlite
+          ];
+        in
+        rec
+        {
+          # Executed by `nix build`
+          packages.default = toolchain.buildRustPackage {
+            pname = "hobob";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            buildInputs = deps;
+
+            # For other makeRustPlatform features see:
+            # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cargo-features-cargo-features
+          };
+
+          # Executed by `nix run`
+          apps.default = utils.lib.mkApp { drv = packages.default; };
+
+          # Used by `nix develop`
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              (with toolchain; [
+                cargo
+                rustc
+                rustLibSrc
+              ])
+              clippy
+              rustfmt
+              pkg-config
+            ] ++ deps;
+
+            # Specify the rust-src path (many editors rely on this)
+            RUST_SRC_PATH = "${toolchain.rustLibSrc}";
+
+            shellHook = ''
+              exec $SHELL
+            '';
+          };
+        }
+      );
+}
