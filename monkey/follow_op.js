a0e7c5cd74b509d2957798c8dca262709e492708
lifeich1 <lifeich0@gmail.com>
2024-12-08 05:08:09 +0800

    :bug: monkey: no follow btn if multi authors

--- monkey/follow_op.js
+++ monkey/follow_op.js
@@ -20,39 +20,41 @@
   const id = searchid();
 
   if (id != 0) {
-    GM.xmlHttpRequest({
-      url: "http://my-pi:3731/get/user/" + id,
-      responseType: "json",
-      onerror: e => { console.error('get user:', e); },
-      onload: resp => {
-        const user_info = resp.response;
-        console.log('get/user/'+ id, 'resp is:', user_info);
-        if (typeof(user_info) != 'string') {
-          $('span.follow-btn-inner')[0].hidden=true;
-          console.log('已关注');
-          return;
-        }
-        $('span.follow-btn-inner')[0].onclick = (() => {
-          console.log('op follow', id);
-          GM.xmlHttpRequest({
-            method: 'POST',
-            url: "http://my-pi:3731/op/follow",
-            headers: {
-              "Content-Type": "application/json"
-            },
-            data: JSON.stringify({
-              enable: true,
-              uid: parseInt(id),
-            }),
-            onerror: e => { console.error('get user:', e); },
-            onload: resp => {
-              console.log('follow result', resp);
-              $('span.follow-btn-inner')[0].textContent = resp.responseText;
-            },
+    if ($('span.follow-btn-inner') != null && $('span.follow-btn-inner').length > 0) {
+      GM.xmlHttpRequest({
+        url: "http://my-pi:3731/get/user/" + id,
+        responseType: "json",
+        onerror: e => { console.error('get user:', e); },
+        onload: resp => {
+          const user_info = resp.response;
+          console.log('get/user/'+ id, 'resp is:', user_info);
+          if (typeof(user_info) != 'string') {
+            $('span.follow-btn-inner')[0].hidden=true;
+            console.log('已关注');
+            return;
+          }
+          $('span.follow-btn-inner')[0].onclick = (() => {
+            console.log('op follow', id);
+            GM.xmlHttpRequest({
+              method: 'POST',
+              url: "http://my-pi:3731/op/follow",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              data: JSON.stringify({
+                enable: true,
+                uid: parseInt(id),
+              }),
+              onerror: e => { console.error('get user:', e); },
+              onload: resp => {
+                console.log('follow result', resp);
+                $('span.follow-btn-inner')[0].textContent = resp.responseText;
+              },
+            });
           });
-        });
-      },
-    });
+        },
+      });
+    }
 
     function regPlaylist() {
       // https://www.bilibili.com/list/30748886?sort_field=pubdate&spm_id_from=333.1387.0.0&oid=113611783543621&bvid=BV15AqjYiEij
