3f98c751ffcfe8ab0c32e058240ddb5ae732f2b6
lifeich1 <lifeich0@gmail.com>
2024-12-28 17:19:14 +0800

    :rocket: strip devops from release

--- flake.nix
+++ flake.nix
@@ -29,6 +29,9 @@
           version = "0.1.0";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
+          postInstall = ''
+            rm -f $out/bin/xtask # devops, strip from release
+          '';
           buildInputs = deps;
           nativeBuildInputs = with pkgs; [
             pkg-config
